56885cc2a9ec6e77d520707de2c99e6f47a0b63d
navaneethhhk <hknavaneeth@gmail.com>
2025-04-05 09:54:56 +0530

    commit

--- src/test/java/org/ATBSelenium/Replicon/Lab01.java
+++ src/test/java/org/ATBSelenium/Replicon/Lab01.java
@@ -15,7 +15,7 @@ public class Lab01 {
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000));
         driver.get("https://so1.replicon.com");
-        driver.findElement(By.id("CompanyNameTextBox")).sendKeys("Apollopsa2");
+        driver.findElement(By.id("CompanyNameTextBox")).sendKeys("navneeth01");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit']"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("LoginNameTextBox"))).sendKeys("admin");
         driver.findElement(By.id("PasswordTextBox")).sendKeys("Replicon@123");
@@ -23,7 +23,7 @@ public class Lab01 {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"desktop-menu-container\"]/ul[3]/li[3]/a"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"employeesandorganization\"]/ul[4]/li[1]/a"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@value=\"+ Add Location\"]"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class =\"errorField\"]"))).sendKeys("loc-nithu1");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class =\"errorField\"]"))).sendKeys("lo-nithu1");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@type=\"button\" and @value=\"Add\"]"))).click();
     }
 
